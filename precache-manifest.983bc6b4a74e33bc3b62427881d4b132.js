fb9a64148890f97d9bef975133a2fe4c1fdb0e13
Lucac11 <95426597+Lucac11@users.noreply.github.com>
2023-05-21 23:08:07 +0300

    Updates

---
+++ precache-manifest.983bc6b4a74e33bc3b62427881d4b132.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ed9d435e1c05cbcd750",
+    "url": "/robofriends1/static/css/main.32e2ba3d.chunk.css"
+  },
+  {
+    "revision": "6ed9d435e1c05cbcd750",
+    "url": "/robofriends1/static/js/main.6ed9d435.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends1/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "8e205d50b0d204b6bec8",
+    "url": "/robofriends1/static/css/2.37f1d559.chunk.css"
+  },
+  {
+    "revision": "8e205d50b0d204b6bec8",
+    "url": "/robofriends1/static/js/2.8e205d50.chunk.js"
+  },
+  {
+    "revision": "cceab306f4008bd8471d",
+    "url": "/robofriends1/static/js/runtime~main.cceab306.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends1/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "804fc966478c6c90db114f059edefcc3",
+    "url": "/robofriends1/index.html"
+  }
+];
